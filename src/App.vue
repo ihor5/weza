92c9085b082bdb94745ae9050fb0da49f0206db5
Ihor5 <rachenko753@gmail.com>
2021-11-23 13:05:07 +0200

    create-api

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  data() {
+    return {
+      APIkey: "45b9e8a006f60807907c5f70acd507aa",
+      city: "Kiev",
+    };
+  },
+  created () {
+    this.getCurrentWeather();
+  },
+  methods:{
+    getCurrentWeather() {
+      axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&APPID=${this.APIkey}`)
+      .then((res) => {
+        console.log(res.data);
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Quicksand" sans-serif;
+}
+</style>
